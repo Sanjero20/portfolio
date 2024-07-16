9c52cd3643d31e5262be48d8628acbfbeafa7a2e
Sanjero20 <gelosantos72@gmail.com>
2024-07-17 01:31:09 +0800

    chore: install tailwind-typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,15 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {},
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: true,
+    themes: ["light", "dark"],
   },
-  plugins: [require('daisyui')],
 };
 export default config;
