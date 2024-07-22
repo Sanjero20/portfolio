71c9665e161cae8a2a2fa5ccc3326fc10244346e
Sanjero20 <gelosantos72@gmail.com>
2024-07-22 14:15:08 +0800

    feat: add more projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,10 @@
 import CardProject from "@/components/CardProject";
-import { PROJECTS } from "@/data/projects";
+import { COLLAB_PROJECTS, PROJECTS } from "@/data/projects";
 
 const ProjectsPage = () => {
   return (
     <div className="flex flex-col gap-2">
-      <p>Here is a list of my projects that i worked on and took apart on</p>
+      <h2 className="sm:text-center">Solo Projects</h2>
 
       {/* Solo Project */}
       <div className="grid gap-4 sm:grid-cols-2">
@@ -19,7 +19,21 @@ const ProjectsPage = () => {
         ))}
       </div>
 
-      {/* Collaboration Project */}
+      <div className="divider mb-0" />
+
+      <h2 className="sm:text-center">Collab Projects</h2>
+
+      <div className="grid gap-4 sm:grid-cols-2">
+        {COLLAB_PROJECTS.map((project, index) => (
+          <CardProject
+            key={index}
+            title={project.title}
+            description={project.description}
+            imgSrc={project.imgSrc}
+            imgAlt={project.imgAlt}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,6 +5,7 @@ export interface ProjectType {
   imgAlt: string;
   tech: string[];
   features: string[];
+  responsibilities?: string[]; // for project collaborations
   links: {
     code: string;
     live: string;
@@ -31,11 +32,45 @@ export const PROJECTS: ProjectType[] = [
       "An interactive learning platform for memorizing the geolocation of the Philippines",
     imgSrc: "/geo-trainer-ph.png",
     imgAlt: "geo trainer",
-    tech: [],
+    tech: ["NextJS", "TypeScript", "Tailwind"],
     features: ["d", "e", "f"],
     links: {
       code: "https://github.com/Sanjero20/geo-trainer",
       live: "https://geo-trainer-ph.vercel.app/",
     },
   },
+  {
+    title: "Mini Message Board",
+    description: "A simple application for sending messages online",
+    imgSrc: "/message-board.png",
+    imgAlt: "message board",
+    tech: ["React", "Tailwind", "TypeScript", "Express JS ", "MongoDB"],
+    features: ["d", "e", "f"],
+    responsibilities: [
+      "Implemented the User Interface",
+      "Integrated the CRUD api developed by the backend developer",
+    ],
+    links: {
+      code: "https://github.com/Sanjero20/mini-message-board",
+      live: "https://mini-message-board-bsor.onrender.com/",
+    },
+  },
 ];
+
+export const COLLAB_PROJECTS: ProjectType[] = [
+  {
+    title: "Dash Scheduler",
+    description:
+      "A system for managing faculty schedules to avoid conflicting room utilizations",
+    imgSrc: "/dash-scheduler.png",
+    imgAlt: "dash scheduler",
+    tech: ["React", "Tailwind", "TypeScript", "ExpressJS", "SQLite"],
+    features: ["a", "b", "c"],
+    links: {
+      code: "https://github.com/Sanjero20/dash-scheduler",
+      live: "https://dash-scheduler.onrender.com/",
+    },
+  },
+];
+
+export const HIGHLIGHTED_PROJECT = [PROJECTS[0], PROJECTS[1]];
