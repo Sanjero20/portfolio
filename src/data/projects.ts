ccbe601772edeac274ccbaf075a621f5b2089ac9
Sanjero20 <gelosantos72@gmail.com>
2024-07-22 17:03:33 +0800

    chore: add more project information

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -20,7 +20,13 @@ export const PROJECTS: ProjectType[] = [
     imgSrc: "/job-tracker.png",
     imgAlt: "job tracker",
     tech: ["React", "Tailwind", "TypeScript", "ExpressJS", "PostgreSQL"],
-    features: ["a", "b", "c"],
+    features: [
+      "Manage account information and data",
+      "User authentication using JWT Tokens",
+      "Add/Update/Delete job applications details",
+      "Edit interview schedules dates",
+      "Graphs and statistics about job statuses",
+    ],
     links: {
       code: "https://github.com/Sanjero20/job-tracker",
       live: "https://job-tracker-0zfp.onrender.com/",
@@ -32,8 +38,12 @@ export const PROJECTS: ProjectType[] = [
       "An interactive learning platform for memorizing the geolocation of the Philippines",
     imgSrc: "/geo-trainer-ph.png",
     imgAlt: "geo trainer",
-    tech: ["NextJS", "TypeScript", "Tailwind"],
-    features: ["d", "e", "f"],
+    tech: ["NextJS", "TypeScript", "Tailwind", "Leaflet"],
+    features: [
+      "Hightlight a specific region of the map",
+      "Check provice name by hovering through the map",
+      "Time-based interactive map quiz",
+    ],
     links: {
       code: "https://github.com/Sanjero20/geo-trainer",
       live: "https://geo-trainer-ph.vercel.app/",
@@ -44,12 +54,8 @@ export const PROJECTS: ProjectType[] = [
     description: "A simple application for sending messages online",
     imgSrc: "/message-board.png",
     imgAlt: "message board",
-    tech: ["React", "Tailwind", "TypeScript", "Express JS ", "MongoDB"],
-    features: ["d", "e", "f"],
-    responsibilities: [
-      "Implemented the User Interface",
-      "Integrated the CRUD api developed by the backend developer",
-    ],
+    tech: ["React", "Tailwind", "TypeScript", "Express JS", "MongoDB"],
+    features: ["Send messages with or without a username."],
     links: {
       code: "https://github.com/Sanjero20/mini-message-board",
       live: "https://mini-message-board-bsor.onrender.com/",
@@ -64,8 +70,25 @@ export const COLLAB_PROJECTS: ProjectType[] = [
       "A system for managing faculty schedules to avoid conflicting room utilizations",
     imgSrc: "/dash-scheduler.png",
     imgAlt: "dash scheduler",
-    tech: ["React", "Tailwind", "TypeScript", "ExpressJS", "SQLite"],
-    features: ["a", "b", "c"],
+    tech: [
+      "React",
+      "Tailwind",
+      "Zustand",
+      "React-Query",
+      "TypeScript",
+      "ExpressJS",
+      "SQLite",
+    ],
+    features: [
+      "Admin panel for managing faculty accounts",
+      "Highlight conflicting schedules based on room utilization",
+      "Printable schedules for each faculty or class sections",
+    ],
+    responsibilities: [
+      "Implemented the user interface.",
+      "Integrated the CRUD api developed by the backend developer.",
+      "Transform an excel document to HTML tables format.",
+    ],
     links: {
       code: "https://github.com/Sanjero20/dash-scheduler",
       live: "https://dash-scheduler.onrender.com/",
